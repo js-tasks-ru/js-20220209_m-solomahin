5c6f89f33256a3090eab34fded7926f32de0156d
Максим Соломахин <maksim@MacBook-Pro-Maksim.local>
2022-02-12 19:37:16 +0300

    create sortString function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,5 +1,4 @@
-/**
- * sortStrings - sorts array of string by two criteria "asc" or "desc"
+ /* sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
