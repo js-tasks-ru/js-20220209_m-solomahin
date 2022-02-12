daad7c7a29751454435d0e99a2362511e5fd5f1d
Maxim Solomahin <m.solomahin@mail.ru>
2022-02-12 19:57:46 +0300

    create sortString

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,13 +1,14 @@
- /* sortStrings - sorts array of string by two criteria "asc" or "desc"
- * @param {string[]} arr - the array of strings
- * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
- * @returns {string[]}
- */
+/* sortStrings - sorts array of string by two criteria "asc" or "desc"
+* @param {string[]} arr - the array of strings
+* @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+* @returns {string[]}
+*/
 export function sortStrings(arr, param = 'asc') {
-    let result = [...arr].sort((a,b) =>
-      a.localeCompare(b, 'ru-en-u-kf-upper'));
-    return param === "asc" ? result : result.reverse();
+  let result = [...arr].sort((a, b) =>
+    a.localeCompare(b, 'ru-en-u-kf-upper'));
+  return param === "asc" ? result : result.reverse();
 }
+
 
 
 
